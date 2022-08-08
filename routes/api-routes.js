b829dfdf790d56cae49e68b03e049adf60ebf7a1
PhilipK-webdev <philipk179@gmail.com>
2022-08-08 21:39:20 +0300

    edit:another test

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -6,7 +6,7 @@ router.get("/api/movies/:movie/:page", async (req, res) => {
   const { movie, page } = req.params;
   const data = await axios({
     method: "GET",
-    url: `https://omdbapi.com/?s=${movie}&type=movie&apikey=${process.env.API_KEY}&page=${page}`,
+    url: `https://omdbapi.com/?s=${movie}&type=movie&apikey=${process.env.API_KEY_OMDB}&page=${page}`,
   });
   const movies = await data.data;
   res.json(movies).status(200);
@@ -15,7 +15,7 @@ router.get("/api/movies/:movie/:page", async (req, res) => {
 router.get("/api/movie/:id", async (req, res) => {
   const data = await axios({
     method: "GET",
-    url: `https://omdbapi.com/?i=${req.params.id}&type=movie&apikey=${process.env.API_KEY}`,
+    url: `https://omdbapi.com/?i=${req.params.id}&type=movie&apikey=${process.env.API_KEY_OMDB}`,
   });
   const movies = await data.data;
   res.json(movies).status(200);
