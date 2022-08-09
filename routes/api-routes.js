ce3119551f2a21b697820d3333c65bf1cea39e8c
Philip K <philipk179@gmail.com>
2022-08-09 10:11:51 +0300

    add:create new route for 10 movies

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,6 +2,37 @@ require("dotenv").config();
 const router = require("express").Router();
 const axios = require("axios");
 
+const MOVIES = [
+  "Inception",
+  "The Green Mile",
+  "Whiplash",
+  "Avengers: Infinity War",
+  "Captain America: The First Avenger",
+  "Captain America: The Winter Soldier",
+  "Doctor Strange",
+  "Avengers:Endgame",
+  "Bad Boys",
+  "Independence Day",
+];
+
+router.get("/api/movies", async (req, res) => {
+  const movieArray = [];
+  for (let index = 0; index < MOVIES.length; index++) {
+    const response = await axios({
+      method: "GET",
+      url: `https://omdbapi.com/?t=${MOVIES[index]}&type=movie&apikey=${process.env.API_KEY_OMDB}`,
+    });
+    const movieObject = {
+      title: await response.data.Title,
+      imdbRating: await response.data.imdbRating,
+      imdbID: await response.data.imdbID,
+      poster: await response.data.Poster,
+      plot: await response.data.Plot,
+    };
+    movieArray.push(movieObject);
+  }
+  res.json(movieArray).status(200);
+});
 router.get("/api/movies/:movie/:page", async (req, res) => {
   const { movie, page } = req.params;
   const data = await axios({
